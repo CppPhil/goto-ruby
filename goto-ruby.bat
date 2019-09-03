812c33f4b0880e7f517497192b66514c55e4d834
CppPhil <CppPhil@users.noreply.github.com>
2019-09-03 15:31:15 +0200

    commit

--- goto-ruby.bat
+++ goto-ruby.bat
@@ -2,7 +2,9 @@
 
 REM TODO check if Ruby is already there
 
-%~dp0%windows_data\7z1900.exe /S /D="%~dp0%windows_data\7z"
+DIR=%~dp0
+
+%DIR%\windows_data\7z1900.exe /S /D="%DIR%\windows_data\7z"
 
 
 
