90cd8fdabbb780c78cfbf46c597e5a67ae51aff7
CppPhil <CppPhil@users.noreply.github.com>
2019-09-03 18:50:54 +0200

    uaoee

--- example/cpp_app/main.cpp
+++ example/cpp_app/main.cpp
@@ -1,13 +1,10 @@
-//#include <iostream>
-//#include <string>
-//#include <print.h>
-
-extern "C" void print(const char* string);
+#include <iostream>
+#include <string>
+#include <print.h>
 
 int main()
 {
-    //const std::string str{"test"};
-    //std::cout << "C++ here: \"" << str << "\"!\n";
-    //print(str.data());
-    print("CUNT");
+    const std::string str{"test"};
+    std::cout << "C++ here: \"" << str << "\"!\n";
+    print(str.data());
 }
