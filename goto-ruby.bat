e07646a265158d0088052e478ae403874797e0dc
CppPhil <CppPhil@users.noreply.github.com>
2019-09-03 15:55:27 +0200

    aoeutnuhao

--- goto-ruby.bat
+++ goto-ruby.bat
@@ -8,7 +8,7 @@ mkdir %DIR%\windows_data\7z
 %DIR%\windows_data\7z1900.exe /S /D="%DIR%\windows_data\7z"
 
 mkdir %DIR%\windows_data\ruby
-%DIR%\windows_data\7z\7z.exe x %DIR%\windows_data\rubyinstaller-2.6.3-1-x86.7z -o%DIR%\windows_data\ruby
+%DIR%\windows_data\7z\7z.exe x %DIR%\windows_data\rubyinstaller-2.6.3-1-x86.7z -y -o%DIR%\windows_data\ruby > nul
 
 If "%~1"=="" goto HELP
 
