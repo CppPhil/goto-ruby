b5cc020d316ee46f21bb46efe55921cccf2c376c
CppPhil <CppPhil@users.noreply.github.com>
2019-09-03 18:54:36 +0200

    update

--- example/build.sh
+++ example/build.sh
@@ -3,6 +3,6 @@
 # Directory containing this bash script
 readonly DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-$DIR/../goto-ruby.sh $DIR/scripts/build.rb $1
+$DIR/../goto-ruby.sh $DIR/scripts/build.rb "$@"
 
 exit 0
